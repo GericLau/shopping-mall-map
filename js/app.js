4860b45e53f6894338d4da2338daa5f4d4e52956
Joey <guoxianl@gmail.com>
2017-05-26 00:50:02 +0800

    add search or filter function

--- js/app.js
+++ js/app.js
@@ -48,17 +48,39 @@ var initialMall = [
         }
     }
 ]
+
 var Mall = function(data) {
     this.title = ko.observable(data.title);
 }
 
 var ViewModel = function() {
     var that = this;
-    this.mallList = ko.observableArray([]);
+    this.mallList = ko.observableArray([]),
+    this.filter = ko.observable(""),
+    this.resultMall = ko.observableArray([]);
+
 
     initialMall.forEach(function(mallItem){
         that.mallList.push( new Mall(mallItem) )
     });
+
+    this.filteredMalls = ko.computed(function() {
+        var filterText = this.filter().toLowerCase();
+        // reset the filter result for every query
+        that.resultMall = ko.observableArray([]);
+
+        if (!filterText) {
+            return that.mallList();
+        } else {
+            // push the mall into result match query
+            initialMall.forEach(function(mallItem){
+                if(mallItem.title.toLowerCase().indexOf(that.filter().toLowerCase()) >= 0) {
+                    that.resultMall.push( new Mall(mallItem) )
+                }
+            });
+            return that.resultMall();
+        }
+    },this);
 }
 
 ko.applyBindings(new ViewModel());
